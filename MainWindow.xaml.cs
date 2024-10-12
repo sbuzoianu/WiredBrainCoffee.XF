b2ec9ed24712adc19502f11778382932bb9daadc
Stefan Buzoianu <stefan.buzoianu@napa.fi>
2024-10-12 11:38:03 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WiredBrainCoffee.XF {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void Btn_Add_Customer(object sender, RoutedEventArgs e) {
+            MessageBox.Show("User added!");
+        }
+    }
+}
